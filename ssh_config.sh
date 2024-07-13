9a3976f7c7e15eead9ab6a08e6cfeda6e7998144
ander-Sgr <andersonbarreto2001@gmail.com>
2024-07-13 13:07:32 +0200

    adding ssh config with ufw

--- ssh_config.sh
+++ ssh_config.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-ssh-keygen -t rsa -N "" -f /home/vagrant/.ssh/id_rsa
-cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
-echo "Host 192.168.100.*" >> /home/vagrant/.ssh/config
-echo "    StrictHostKeyChecking no" >> /home/vagrant/.ssh/config
-echo "    UserKnownHostsFile=/dev/null" >> /home/vagrant/.ssh/config
+# Instalar ufw si no está instalado
+sudo apt-get update
+sudo apt-get install -y ufw
+
+# Resetear las reglas por defecto
+sudo ufw --force reset
+
+# Permitir SSH solo desde proxy_serv
+sudo ufw allow from 192.168.100.10 to any port 22
+
+# sudo ufw allow from 192.168.1.133 to any port 22
+
+sudo ufw allow 8080 
+
+# Denegar SSH desde cualquier otra dirección
+sudo ufw deny 22
+
+# Habilitar ufw
+sudo ufw --force enable
+
+# Verificar el estado de ufw
+sudo ufw status verbose
